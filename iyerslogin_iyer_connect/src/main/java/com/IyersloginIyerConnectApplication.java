d3762056bed318987a5112af8429058c82ceabc4
Admin <Admin@DESKTOP-UR86AL5>
2025-09-25 13:35:24 +0530

    testing iyer connect

---
+++ iyerslogin_iyer_connect/src/main/java/com/IyersloginIyerConnectApplication.java
@@ -0,0 +1,13 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IyersloginIyerConnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IyersloginIyerConnectApplication.class, args);
+	}
+
+}
---
+++ iyerslogin_iyer_connect/src/test/java/com/IyersloginIyerConnectApplicationTests.java
@@ -0,0 +1,13 @@
+package com;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IyersloginIyerConnectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
